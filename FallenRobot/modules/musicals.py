4b42f8171242e6c52bc814eab33467e4e92daef3
Yong su <107544831+helplessg@users.noreply.github.com>
2022-08-17 01:48:33 +0530

    Update musicals.py

--- FallenRobot/modules/musicals.py
+++ FallenRobot/modules/musicals.py
@@ -1,6 +1,8 @@
 __mod_name__ = "Mᴜsɪᴄ"
 
 __help__ = """
- ❍ /song <song name>*:* Uploads the song in it's best quality available
+ ❍ /song <song name>*:* ᴜᴘʟᴏᴀᴅs ᴛʜᴇ sᴏɴɢ ɪɴ ɪᴛ's ʙᴇsᴛ ǫᴜᴀʟɪᴛʏ ᴀᴠᴀɪʟᴀʙʟᴇ
+ ❍ /vsong <song name>*:* ꜰᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴠɪᴅᴇᴏ sᴏɴɢ ᴏʀ ᴠɪᴅᴇᴏ ꜰʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ ɪɴ ʙᴇsᴛ ǫᴜᴀʟɪᴛʏ
  💡Ex: `/song Faded Alan Walker`
+ 💡Ex: `/vsong Faded Alan Walker`
 """
