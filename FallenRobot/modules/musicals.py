1d21052482f8657b5758c6d22e0c372cbec404e8
Yong su <107544831+helplessg@users.noreply.github.com>
2022-08-17 01:39:16 +0530

    Delete musicals.py

--- FallenRobot/modules/musicals.py
+++
@@ -1,8 +0,0 @@
-__mod_name__ = "Mᴜsɪᴄ"
-
-__help__ = """
- ❍ /song <song name>*:**ᴜᴘʟᴏᴀᴅs ᴛʜᴇ sᴏɴɢ ɪɴ ɪᴛ's ʙᴇsᴛ ǫᴜᴀʟɪᴛʏ ᴀᴠᴀɪʟᴀʙʟᴇ**
- ❍ /vsong <song name>*:**ꜰᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴠɪᴅᴇᴏ sᴏɴɢ ᴏʀ ᴠɪᴅᴇᴏ ꜰʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ ɪɴ ʙᴇsᴛ ǫᴜᴀʟɪᴛʏ** 🎗
- 📍**Ex**: `/song Faded Alan Walker`
- 📍**Ex**: `/vsong Faded Alan Walker'
-"""
