21d828dd1ff015f021498d25a54adb3491a831a4
Yong su <107544831+helplessg@users.noreply.github.com>
2022-08-17 13:48:13 +0530

    Update karma.py

--- FallenRobot/modules/karma.py
+++ FallenRobot/modules/karma.py
@@ -60,7 +60,7 @@ async def upvote(_, message):
     new_karma = {"karma": karma}
     await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     await message.reply_text(
-        f"Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}"
+        f"Iɴᴄʀᴇᴍᴇɴᴛᴇᴅ Kᴀʀᴍᴀ ᴏғ {user_mention} Bʏ 1 \n❤ Tᴏᴛᴀʟ Pᴏɪɴᴛs: {karma}"
     )
 
 
@@ -96,7 +96,7 @@ async def upvote(_, message):
     new_karma = {"karma": karma}
     await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     await message.reply_text(
-        f"Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}"
+        f"Iɴᴄʀᴇᴍᴇɴᴛᴇᴅ Kᴀʀᴍᴀ ᴏғ {user_mention} Bʏ 1 \n❤ Tᴏᴛᴀʟ Pᴏɪɴᴛs: {karma}"
     )
 
 
@@ -133,7 +133,7 @@ async def downvote(_, message):
     new_karma = {"karma": karma}
     await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     await message.reply_text(
-        f"Decremented Karma Of {user_mention} By 1 \nTotal Points: {karma}"
+        f"ᴅᴇᴄʀᴇᴍᴇɴᴛᴇᴅ ᴋᴀʀᴍᴀ Oғ {user_mention} Bʏ 1 \n💔 Tᴏᴛᴀʟ Pᴏɪɴᴛs: {karma}"
     )
 
 
@@ -142,12 +142,12 @@ async def downvote(_, message):
 async def karma(_, message):
     chat_id = message.chat.id
     if not message.reply_to_message:
-        m = await message.reply_text("Analyzing Karma...Will Take 10 Seconds")
+        m = await message.reply_text("Aɴᴀʟʏᴢɪɴɢ Kᴀʀᴍᴀ...Wɪʟʟ Tᴀᴋᴇ 𝟷𝟶 Sᴇᴄᴏɴᴅs....")
         karma = await get_karmas(chat_id)
         if not karma:
-            await m.edit("No karma in DB for this chat.")
+            await m.edit("Nᴏ ᴋᴀʀᴍᴀ ɪɴ DB ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ")
             return
-        msg = f"**Karma list of {message.chat.title}:- **\n"
+        msg = f"**ᴋᴀʀᴍᴀ ʟɪsᴛ Oғ {message.chat.title}:- **\n"
         limit = 0
         karma_dicc = {}
         for i in karma:
@@ -158,7 +158,7 @@ async def karma(_, message):
                 sorted(karma_dicc.items(), key=lambda item: item[1], reverse=True)
             )
         if not karma_dicc:
-            await m.edit("No karma in DB for this chat.")
+            await m.edit("Nᴏ ᴋᴀʀᴍᴀ ɪɴ DB ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ")
             return
         for user_idd, karma_count in karma_arranged.items():
             if limit > 9:
@@ -179,7 +179,7 @@ async def karma(_, message):
         user_id = message.reply_to_message.from_user.id
         karma = await get_karma(chat_id, await int_to_alpha(user_id))
         karma = karma["karma"] if karma else 0
-        await message.reply_text(f"**Total Points**: __{karma}__")
+        await message.reply_text(f"**Tᴏᴛᴀʟ Pᴏɪɴᴛs**: __{karma}__")
 
 
 @app.on_message(filters.command("karma") & ~filters.private)
